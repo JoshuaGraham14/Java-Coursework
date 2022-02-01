95c99920b0c1f6da70afdeeaf1d3878298fc970f
Joshua Graham <sc21jg@leeds.ac.uk>
2022-02-01 12:40:11 +0000

    finshed program

--- exercises/ex1/other/MilesToKm.java
+++ exercises/ex1/other/MilesToKm.java
@@ -7,5 +7,9 @@ public class MilesToKm
         System.out.print("Enter distance in miles: ");
         Scanner input = new Scanner(System.in);
         double distanceMiles = input.nextDouble();
+
+        double distanceKm = distanceMiles * (8.0/5.0);
+        System.out.printf("%.1f miles = %.1f km \n ", distanceMiles, distanceKm);
+
     }
 }
