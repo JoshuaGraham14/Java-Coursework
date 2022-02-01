b602d03db99f902bccaf10550648241c2b146b12
Joshua Graham <sc21jg@leeds.ac.uk>
2022-02-01 12:34:47 +0000

    Added code to obtain distance from user

--- exercises/ex1/other/MilesToKm.java
+++ exercises/ex1/other/MilesToKm.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class MilesToKm
 {
     public static void main(String[] args)
     {
-        
+        System.out.print("Enter distance in miles: ");
+        Scanner input = new Scanner(System.in);
+        double distanceMiles = input.nextDouble();
     }
 }
