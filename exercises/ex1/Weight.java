a5087d68f01793e388a454a99277d75b146de90f
Joshua Graham <sc21jg@leeds.ac.uk>
2022-02-01 13:13:43 +0000

    Final conversion

--- exercises/ex1/Weight.java
+++ exercises/ex1/Weight.java
@@ -8,5 +8,11 @@ public class Weight
         Scanner input = new Scanner(System.in);
         double weightKilograms = input.nextDouble();
         input.close();
+
+        double weightOunces = weightKilograms * 35.274;
+        int weightPounds = (int) Math.floor(weightOunces/16);
+        weightOunces = weightOunces - 16*weightPounds;
+        
+        System.out.printf("Equivalent imperial weight is %d lb %.1f oz\n", weightPounds, weightOunces);
     }
 }
