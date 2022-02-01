e2771157703b95f61557e02ec7abf8fb0235461d
Joshua Graham <sc21jg@leeds.ac.uk>
2022-02-01 12:58:47 +0000

    Added code to obtain weight from user

--- exercises/ex1/Weight.java
+++ exercises/ex1/Weight.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class Weight 
 {
     public static void main(String[] args) 
     {
-        
+        System.out.print("Enter weight in kilograms: ");
+        Scanner input = new Scanner(System.in);
+        double weightKilograms = input.nextDouble();
+        input.close();
     }
 }
