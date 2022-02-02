4e12b22eddb63561cf11e1451430d60941f7f429
Joshua Graham <sc21jg@leeds.ac.uk>
2022-02-02 10:57:18 +0000

    Added if statements plus error messages

--- exercises/ex3/Spheroid.java
+++ exercises/ex3/Spheroid.java
@@ -11,10 +11,25 @@ public class Spheroid
         double polarRadius = input.nextDouble();
         input.close();
 
+        if(equatorialRadius <= 0)
+        {
+            System.out.println("Error: equatorial radius must be larger than 0");
+            System.exit(1);
+        }
+        if(polarRadius <= 0)
+        {
+            System.out.println("Error: polar radius must be larger than 0");
+            System.exit(1);
+        }
+        if(polarRadius > equatorialRadius)
+        {
+            System.out.println("Error: equatorial radius must be larger than polar radius");
+            System.exit(1);
+        }
         double eccentricity = Math.sqrt(1-(Math.pow(polarRadius, 2)/Math.pow(equatorialRadius, 2)));
         double volume = (4*Math.PI*Math.pow(equatorialRadius, 2)*polarRadius)/3;
 
         System.out.printf("Eccentricity = %.3f\n", eccentricity);
-        System.out.printf("Volume = %g cubic km", volume);
+        System.out.printf("Volume = %g cubic km\n", volume);
     }
 }
