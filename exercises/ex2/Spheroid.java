56d34c0f694ce7ab749c834fb9777ab6db9cce54
Joshua Graham <sc21jg@leeds.ac.uk>
2022-02-02 10:34:50 +0000

    user input

--- exercises/ex2/Spheroid.java
+++ exercises/ex2/Spheroid.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class Spheroid
 {
     public static void main(String[] args) 
     {
-        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter equatorial radius in km: ");
+        double equatorialRadius = input.nextDouble();
+        System.out.print("Enter polar radius in km: ");
+        double polarRadius = input.nextDouble();
+        input.close();
+
     }
 }
