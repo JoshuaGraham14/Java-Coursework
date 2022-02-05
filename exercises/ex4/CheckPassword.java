3fdb4a1cc81c39de6d6c4599175a73852ef5b08d
Joshua Graham <sc21jg@leeds.ac.uk>
2022-02-05 21:41:47 +0000

    Skeleton file

---
+++ exercises/ex4/CheckPassword.java
@@ -0,0 +1,10 @@
+public class CheckPassword {
+  public static boolean longEnough(String password) {
+  }
+
+  public static boolean atLeastTwoDigits(String password) {
+  }
+
+  public static void main(String[] args) {
+  }
+}
