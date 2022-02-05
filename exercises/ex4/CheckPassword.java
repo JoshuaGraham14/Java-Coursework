3f7c17754f5c46ac21f38ed296c1513e73d434c2
Joshua Graham <sc21jg@leeds.ac.uk>
2022-02-05 23:00:18 +0000

    filled in functions

--- exercises/ex4/CheckPassword.java
+++ exercises/ex4/CheckPassword.java
@@ -4,13 +4,29 @@ public class CheckPassword
 {
     public static boolean longEnough(String password) 
     {
-        
-        return true;
+        if (password.length() >= 12)
+        {
+            return true;
+        } 
+        return false;
     }
 
     public static boolean atLeastTwoDigits(String password) 
     {
-        return true;
+        int numOfDigits = 0;
+        for (int i = 0; i < password.length(); i++)
+        {
+            if (Character.isDigit(password.charAt(i)) == true)
+            {
+                numOfDigits++;
+            }
+        }
+
+        if (numOfDigits >= 2)
+        {
+            return true;
+        } 
+        return false;
     }
 
     public static void main(String[] args) 
