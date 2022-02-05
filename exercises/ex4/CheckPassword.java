82f1c7c2426399303cc317ec6228b36a287495b6
Joshua Graham <sc21jg@leeds.ac.uk>
2022-02-05 22:24:53 +0000

    If statements structure

--- exercises/ex4/CheckPassword.java
+++ exercises/ex4/CheckPassword.java
@@ -1,10 +1,32 @@
-public class CheckPassword {
-  public static boolean longEnough(String password) {
-  }
+import javax.lang.model.util.ElementScanner14;
 
-  public static boolean atLeastTwoDigits(String password) {
-  }
+public class CheckPassword 
+{
+    public static boolean longEnough(String password) 
+    {
+        
+        return true;
+    }
 
-  public static void main(String[] args) {
-  }
+    public static boolean atLeastTwoDigits(String password) 
+    {
+        return true;
+    }
+
+    public static void main(String[] args) 
+    {
+        if (longEnough(args[0]) == true & atLeastTwoDigits(args[0]) == true)
+        {
+            System.out.println("Password is valid");
+        }
+        else if(args[0] == null)
+        {
+            System.out.println(System.err);
+            System.exit(1);
+        }
+        else 
+        {
+            System.out.println("Password is not valid");
+        }
+    }
 }
