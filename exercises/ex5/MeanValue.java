2d96498592bca7d0ea1fc99d6189cf499f3e6d7a
Joshua Graham <sc21jg@leeds.ac.uk>
2022-02-09 18:14:02 +0000

    main function implemented

--- exercises/ex5/MeanValue.java
+++ exercises/ex5/MeanValue.java
@@ -2,11 +2,25 @@ public class MeanValue
 {
     public static double meanValue(double[] data) 
     {
-
+        return 0;
     }
 
     public static void main(String[] args) 
     {
-        
+        if(args.length == 0)
+        {
+            System.err.println("Usage: java MeanValue <values...>");
+            System.exit(1);
+        }
+        else 
+        {
+            double[] doubleValues = new double[args.length];
+            for (int i = 0; i < args.length; i++)
+            {
+                doubleValues[i] = Double.parseDouble(args[i]);
+            }
+            double mean = meanValue(doubleValues);
+            System.out.printf("Mean value = %.3f\n", mean);
+        }
     }
 }
