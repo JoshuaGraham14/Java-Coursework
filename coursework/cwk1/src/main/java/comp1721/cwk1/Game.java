96b861be5f3d1ede36d4c844da8d55edcb038ed9
Joshua Graham <sc21jg@leeds.ac.uk>
2022-02-28 20:39:37 +0000

    5.1 - Game constructors implemented

--- coursework/cwk1/src/main/java/comp1721/cwk1/Game.java
+++ coursework/cwk1/src/main/java/comp1721/cwk1/Game.java
@@ -1,20 +1,30 @@
 package comp1721.cwk1;
 
+import java.io.IOException;
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+
 public class Game
 {
     private static int gameNumber;
     private static String target;
 
     // TODO: Implement constructor with String parameter
-    public Game (String filename)
+    public Game (String filename) throws IOException
     {
-        WordList
+        WordList w = new WordList(filename);
+        LocalDate startDate = LocalDate.of(2021, 6, 19);
+        LocalDate today = LocalDate.now();
+
+        gameNumber = (int) ChronoUnit.DAYS.between(startDate, today);
+        target = w.getWord(gameNumber);
     }
 
     // TODO: Implement constructor with int and String parameters
-    public Game (int num, String filename)
+    public Game (int num, String filename) throws IOException
     {
-        
+        WordList w = new WordList(filename);
+        target = w.getWord(num);
     }
 
     // TODO: Implement play() method
