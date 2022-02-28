69aac0224c8accffafd76ad35e530548e4f434bc
Joshua Graham <sc21jg@leeds.ac.uk>
2022-02-28 20:18:38 +0000

    4.2 - Compare with method completed

--- coursework/cwk1/src/main/java/comp1721/cwk1/Guess.java
+++ coursework/cwk1/src/main/java/comp1721/cwk1/Guess.java
@@ -4,6 +4,12 @@ import java.util.Scanner;
 
 public class Guess 
 {
+    public static final String ANSI_RESET_BACKGROUND = "\033[0m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_GREEN_BACKGROUND = "\033[30;102m";
+	public static final String ANSI_YELLOW_BACKGROUND = "\033[30;103m";
+	public static final String ANSI_WHITE_BACKGROUND = "\033[30;107m";
+
     // Use this to get player input in readFromPlayer()
     private static final Scanner INPUT = new Scanner(System.in);
     private static int guessNumber;
@@ -41,7 +47,7 @@ public class Guess
         word = word.toUpperCase();
         for (int i=0; i<5; i++)
         {
-            if(!(word.charAt(0) >= 'A' && word.charAt(0) <= 'Z'))
+            if(!(word.charAt(i) >= 'A' && word.charAt(i) <= 'Z'))
             {
                 isWord = false;
                 break;
@@ -60,13 +66,13 @@ public class Guess
     // TODO: Implement getGuessNumber(), returning an int
     public static int getGuessNumber ()
     {
-        return 0;
+        return guessNumber;
     }
 
     // TODO: Implement getChosenWord(), returning a String
     public static String getChosenWord ()
     {
-        return "";
+        return chosenWord;
     }
 
     // TODO: Implement readFromPlayer()
@@ -78,12 +84,43 @@ public class Guess
     // TODO: Implement compareWith(), giving it a String parameter and String return type
     public static String compareWith (String target)
     {
-        return "";
+        String outputString = "";
+
+        for (int i=0; i<5; i++)
+        {
+            if(chosenWord.charAt(i) == target.charAt(i))
+            {
+                outputString+= ANSI_GREEN_BACKGROUND + chosenWord.charAt(i) + ANSI_RESET_BACKGROUND;
+            }
+            else
+            {
+                boolean isFound = false;
+                for (int j=i+1; j<(5-i); j++)
+                {
+                    if(chosenWord.charAt(i) == target.charAt(j))
+                    {
+                        outputString+= ANSI_YELLOW_BACKGROUND + chosenWord.charAt(i) + ANSI_RESET_BACKGROUND;
+                        isFound = true;
+                        break;
+                    }
+                }
+                if(!isFound)
+                {
+                    outputString+= ANSI_WHITE_BACKGROUND + chosenWord.charAt(i) + ANSI_RESET_BACKGROUND;
+                }
+            }
+        }
+
+        return outputString;
     }
 
     // TODO: Implement matches(), giving it a String parameter and boolean return type
     public static Boolean matches (String target)
     {
+        if (chosenWord == target.toUpperCase())
+        {
+            return true;
+        }
         return false;
     }
 }
