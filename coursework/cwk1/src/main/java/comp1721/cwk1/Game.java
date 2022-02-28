70ce08270a944be81dcf6a3cabdf11494b18d627
Joshua Graham <sc21jg@leeds.ac.uk>
2022-02-28 21:00:07 +0000

    5.2 - Play() method implemented

--- coursework/cwk1/src/main/java/comp1721/cwk1/Game.java
+++ coursework/cwk1/src/main/java/comp1721/cwk1/Game.java
@@ -3,11 +3,15 @@ package comp1721.cwk1;
 import java.io.IOException;
 import java.time.*;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Game
 {
     private static int gameNumber;
     private static String target;
+
+    private static ArrayList<Guess> guessesArray = new ArrayList<Guess>();
 
     // TODO: Implement constructor with String parameter
     public Game (String filename) throws IOException
@@ -30,7 +34,21 @@ public class Game
     // TODO: Implement play() method
     public static void play()
     {
-        
+        System.out.printf("WORDLE %s/n/n", gameNumber);
+        for (int i=1; i<=6; i++)
+        {
+            System.out.printf("Enter guess (%d/6): ", i);
+            Scanner INPUT = new Scanner(System.in);
+            String guess = INPUT.nextLine();
+            INPUT.close();
+            Guess g = new Guess(i, guess);
+            System.out.println(g.compareWith(target));
+            if(g.matches(target))
+            {
+                System.out.print("Well done!");
+                break;
+            }
+        }
     }
 
     // TODO: Implement save() method, with a String parameter
