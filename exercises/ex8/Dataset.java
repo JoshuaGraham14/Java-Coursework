cc6fa72b8467ac19deedb8588a3a5b39e84f9148
Joshua Graham <sc21jg@leeds.ac.uk>
2022-03-06 22:27:51 +0000

    read from file implemented + other files added

--- exercises/ex8/Dataset.java
+++ exercises/ex8/Dataset.java
@@ -1,22 +1,38 @@
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 class Dataset
 {
     ArrayList<Double> data = new ArrayList<Double>();
 
-    public Dataset(String filename)
+    public Dataset(String filename) throws IOException
     {
-        
+        try(Scanner input = new Scanner(Paths.get(filename)))
+        {
+            while (input.hasNextLine())
+            {
+                String line = input.nextLine();
+                data.add(Double.parseDouble(line));
+            }
+        }
     }
 
     public Integer size()
     {
-        return 0;
+        return data.size();
     }
 
     public Double meanValue()
     {
-        return 0.0;
+        Double t = 0.0;
+        for(int i=0; i<size(); i++)
+        {
+            t+=i;
+        }
+        
+        return t/size();
     }
 
 }
