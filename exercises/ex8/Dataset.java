274b43bf08db9946f76d9fdf86ecc523ab7a0d9b
Joshua Graham <sc21jg@leeds.ac.uk>
2022-03-06 22:18:18 +0000

    Inital skeleton

---
+++ exercises/ex8/Dataset.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+class Dataset
+{
+    ArrayList<Double> data = new ArrayList<Double>();
+
+    public Dataset(String filename)
+    {
+        
+    }
+
+    public Integer size()
+    {
+        return 0;
+    }
+
+    public Double meanValue()
+    {
+        return 0.0;
+    }
+
+}
