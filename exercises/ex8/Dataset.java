ae313573c25abe2552a2380f8749325a5e71515d
Joshua Graham <sc21jg@leeds.ac.uk>
2022-03-06 22:29:16 +0000

    size and meanValue methods implemented

--- exercises/ex8/Dataset.java
+++ exercises/ex8/Dataset.java
@@ -29,7 +29,7 @@ class Dataset
         Double t = 0.0;
         for(int i=0; i<size(); i++)
         {
-            t+=i;
+            t+=data.get(i);
         }
         
         return t/size();
