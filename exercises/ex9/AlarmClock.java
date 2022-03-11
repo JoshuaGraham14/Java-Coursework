5037e4ad3857ee5c990c496918e0a2f59604c7f0
Joshua Graham <sc21jg@leeds.ac.uk>
2022-03-11 01:21:37 +0000

    Files created and stub methods created

---
+++ exercises/ex9/AlarmClock.java
@@ -0,0 +1,47 @@
+import javax.xml.crypto.dsig.keyinfo.RetrievalMethod;
+
+public class AlarmClock extends Clock
+{
+    private static int alarmHours;
+    private static int alarmMinutes;
+
+    public AlarmClock()
+    {
+
+    }
+    
+    public AlarmClock(int h, int m)
+    {
+
+    }
+
+    public AlarmClock(int h, int m, int s)
+    {
+
+    }
+
+    public int getAlarmHours()
+    {
+        return 0;
+    }
+
+    public int getAlarmMinutes()
+    {
+        return 0;
+    }
+
+    public void setAlarm(int h, int m)
+    {
+
+    }
+
+    public boolean isRinging()
+    {
+        return true;
+    }
+
+    public void display()
+    {
+
+    }
+}
