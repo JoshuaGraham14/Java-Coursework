77fc860604f5a0576cee8c1ce059467ed7479eef
Joshua Graham <sc21jg@leeds.ac.uk>
2022-05-18 16:23:31 +0100

    Deck skeleton class implemented

--- coursework/cwk2/src/main/java/comp1721/cwk2/Deck.java
+++ coursework/cwk2/src/main/java/comp1721/cwk2/Deck.java
@@ -1,3 +1,40 @@
 package comp1721.cwk2;
 
-// Implement Deck class here
+//Implement Deck class here
+public class Deck extends CardCollection
+{
+    public Deck()
+    {
+        
+    }
+    
+    public int size()
+    {
+        return 0;
+    }
+    
+    public boolean isImpty()
+    {
+        return true;
+    }
+    
+    public boolean contains(Card c)
+    {
+        return true;
+    }
+    
+    public void discard()
+    {
+    
+    }
+    
+    public void deal()
+    {
+    
+    }
+    
+    public void shuffle()
+    {
+    
+    }
+}
