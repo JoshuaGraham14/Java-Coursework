ad0eeca1cee3c01edfe88ac53c37de4cc983167b
Joshua Graham <sc21jg@leeds.ac.uk>
2022-05-22 03:37:29 +0100

    Commented and Doc commented Deck.java

--- coursework/cwk2/src/main/java/comp1721/cwk2/Deck.java
+++ coursework/cwk2/src/main/java/comp1721/cwk2/Deck.java
@@ -11,24 +11,36 @@ import comp1721.cwk2.Card.Suit;
  */
 public class Deck extends CardCollection
 {
+    /**
+   * Class constructor: creates a Deck object.
+   */
     public Deck()
     {
-        super();
+        super(); //Calls the super constructor
 
+        //Loop through each Suit in the enum
         for (Suit s : Suit.values()) 
         { 
+            //Loop through each Rank in the enum
             for (Rank r : Rank.values()) 
             { 
+                //Using every combination of Suit and Rank, create a Card object and add it to the deck.
                 Card c = new Card(r, s);
-                add(c);
+                super.add(c);
             }
         }
     }
     
+    /**
+   * Deals a card from the Deck.
+   * @return Card to be dealt (top card of the Deck)
+   */
     public Card deal()
     {
+        //First check that the deck isn't empty
         if (cards.size() > 0)
         {
+            //Remove the top card from the Deck and then return it:
             Card c = cards.remove(0);
             return c;
         }
@@ -38,6 +50,9 @@ public class Deck extends CardCollection
         }
     }
     
+    /**
+   * Shuffles the Deck (by randomising positions of the cards list).
+   */
     public void shuffle()
     {
         Collections.shuffle(cards);
